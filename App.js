fb4eee25a1143e39fd8c21b88e4cce847733ab92
WwangWeiqi <442056022@qq.com>
2018-11-09 14:56:20 -0800

    test native-languages, retrieve language of locale device

--- App.js
+++ App.js
@@ -10,7 +10,8 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import I18n from './i18n/i18n';
 import {Button,Alert} from 'react-native';
-
+//import DeviceInfo from 'react-native-device-info'
+import RNLanguages from 'react-native-languages';
 //import LocaleSet from 'react-native-i18n';
 //import { getLanguages } from 'react-native-i18n';
 
@@ -47,6 +48,10 @@ toggleLanguage = () =>{
   this.setState({greeting: swit });*/
   }
 
+PhoneLanguage = () => {
+ Alert.alert(RNLanguages.language);
+}
+
 
   render() {
      console.log("render");
@@ -54,8 +59,8 @@ toggleLanguage = () =>{
       <View style={styles.container}>
         <Text style={styles.welcome}>{I18n.t('greeting')}</Text>
         <Text style={styles.instructions}>{I18n.t('name')}</Text>
-        <Button style={{color: 'white'}} title={I18n.t('btnContent')} onPress = {this.toggleLanguage}></Button>
-
+        <Button  title={I18n.t('btnContent')} onPress = {this.toggleLanguage}></Button>
+         <Button title='本机语言' onPress = {this.PhoneLanguage}></Button>
       </View>
     );
   }
